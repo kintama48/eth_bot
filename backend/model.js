1ac30072c05a4c93cfd064b27a9ecbd7f4a55f41
Abdullah Baig <65843525+kintama48@users.noreply.github.com>
2022-01-07 04:09:29 +0500

    Update model.js

--- backend/model.js
+++ backend/model.js
@@ -3,37 +3,35 @@ const { Pool, Client } = require('pg');
 const connectionString = 'postgres://hamzaasaad:@localhost:5432/hamzaasaad'
 
 let pool = new Pool({
-    user: "hamzaasaad",
-    host: "localhost",
-    database: "hamzaasaad",
-    password: "",
-    port: 5431,
+  user: "bpksohjnqxghia",
+  host: "ec2-34-230-133-163.compute-1.amazonaws.com",
+  database: "d91ok380e2boea",
+  password: "391cebb8cfc0080ac7f81493ebfa4e6b3b60e081da74b792ef0f9f1e0455a578",
+  port: 5432
 })
+
+// let pool = new Pool({
+//     user: "hamzaasaad",
+//     host: "localhost",
+//     database: "hamzaasaad",
+//     password: "",
+//     port: 5431,
+// })
+
 pool.connect();
 
 pool.query('SELECT NOW()', (err, res) => {
     if(err){
-        console.log('Database connection failed',err);
+        console.log('Database connection failed', err);
     }
     else {
         console.log('Database connected!');
     }
 });
 
-
-
-
-/*
-user: "bpksohjnqxghia",
-  host: "ec2-34-230-133-163.compute-1.amazonaws.com",
-  database: "d91ok380e2boea",
-  password: "391cebb8cfc0080ac7f81493ebfa4e6b3b60e081da74b792ef0f9f1e0455a578",
-  port: 5432,*/
-
-
 async function checkAddressExists(discordID){
     const select = `
-        SELECT * FROM users where discord_id = $1
+        SELECT * FROM public.users where discord_id=$1
     `;
     const vals=[discordID]
     const result = await pool.query(select,vals);
